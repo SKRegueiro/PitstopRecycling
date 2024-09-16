e9283b4a2a45eea14e13c163f864ac0f8b0dd83c
Sergio Camiño <scaminoregueiro@gmail.com>
2024-09-16 07:45:40 +0200

    added image viewer

--- app/fuelView.tsx
+++ app/fuelView.tsx
@@ -1,12 +1,17 @@
 import { decode } from "base64-arraybuffer";
 import React, { useRef, useState } from "react";
-import { Image, Keyboard, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
-import { Button, DateTimePicker, NumberInput, NumberInputData, TextField } from "react-native-ui-lib";
+import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import { Button, DateTimePicker, Modal, NumberInput, NumberInputData, Text, TextField } from "react-native-ui-lib";
 import { CameraView, useCameraPermissions } from "expo-camera";
 import Label from "react-native-ui-lib/src/components/textField/Label";
 import { Entypo } from "@expo/vector-icons";
 import { supabase } from "@/lib/supabase";
 import insertFuel from "@/services/fuel/insertFuel";
+import ImageViewer from "@/components/ImageViewer";
+import useProfile from "@/lib/hooks/useProfile";
+import Toast from "react-native-toast-message";
+import { router } from "expo-router";
+import Routes from "@/constants/Routes";
 
 const FuelView = () => {
   const [amount, setAmount] = useState("");
@@ -15,9 +20,13 @@ const FuelView = () => {
   const cameraRef = useRef(null);
   const [isCameraOpen, setIsCameraOpen] = useState(false);
   const [picture, setPicture] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { profile } = useProfile();
 
+  //TODO: clean up the external calls. Meter loading
   const onSubmit = async () => {
     try {
+      setLoading(true);
       supabase.storage
         .from("public_fuel")
         .upload("picture" + date, decode(picture?.base64), {
@@ -27,47 +36,59 @@ const FuelView = () => {
         .then(async (bucketResult) => {
           await insertFuel({
             createdAt: date.toISOString(),
-            employeeId: "1",
+            employeeId: profile?.id.toString(),
             receiptImageId: bucketResult?.data?.id,
             cost: amount
           });
+          Toast.show({
+            type: "success",
+            text1: "Success!",
+            text2: "Your ticket fuel has been uploaded!"
+          });
+          router.navigate(Routes.Home);
         });
     } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: "Something went wrong. Try again"
+      });
       console.log(error);
     }
+    setLoading(false);
+  };
+
+  const onTakePicture = async () => {
+    await cameraRef?.current
+      ?.takePictureAsync({ base64: true })
+      .then((photo) => setPicture(photo))
+      .catch((error) => console.log(error))
+      .finally(() => setIsCameraOpen(false));
+  };
+
+  const onRemovePicture = () => {
+    setPicture(null);
   };
 
   if (!permission || !permission.granted) {
     return (
       <View style={styles.container}>
         <TextField style={styles.message} label={"We need your permission to show the camera"}></TextField>
-        <Button onPress={requestPermission} label="Grant permission" />
+        <Button enableShadow onPress={requestPermission} label="Grant permission" />
       </View>
     );
   }
 
-  //TODO: upload picture. need to convert to base64. show picture on app so they can check its correct. Add also a upload from library option. see if paid or not
-  if (isCameraOpen && permission.granted) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Capture the ticket </Text>
-        <CameraView style={styles.camera} facing={"back"} ref={cameraRef} />
-        <Button
-          label={"Done"}
-          onPress={async () => {
-            await cameraRef?.current
-              ?.takePictureAsync({ base64: true })
-              .then((photo) => setPicture(photo))
-              .catch((error) => console.log(error))
-              .finally(() => setIsCameraOpen(false));
-          }}
-        ></Button>
-      </View>
-    );
-  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Modal visible={isCameraOpen && permission.granted}>
+        <View style={{ paddingVertical: 30, paddingHorizontal: 10, ...styles.container }}>
+          <Text style={styles.text}>Capture the ticket clearly </Text>
+          <CameraView style={styles.camera} facing={"back"} ref={cameraRef} />
+          <Button enableShadow label={"Done"} onPress={onTakePicture}></Button>
+        </View>
+      </Modal>
 
-  if (!isCameraOpen) {
-    return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View style={styles.container}>
           <View style={styles.inputs}>
@@ -86,24 +107,33 @@ const FuelView = () => {
               <DateTimePicker style={styles.textField} value={date} onChange={setDate} mode={"date"}></DateTimePicker>
             </View>
           </View>
-          {picture && <Image style={{ width: 200, height: 200 }} source={{ uri: picture?.uri }} />}
-          <Button label={"Photo receipt"} onPress={() => setIsCameraOpen(true)}>
+          <ImageViewer path={picture?.uri} onRemove={onRemovePicture} />
+          <Button
+            enableShadow
+            label={!picture ? "Receipt proof" : "Change photo"}
+            onPress={() => setIsCameraOpen(true)}
+          >
             <Entypo style={{ marginRight: 10 }} name="camera" size={24} color="white" />
           </Button>
-          <Button label={"Submit"} disabled={!amount} onPress={() => onSubmit()} />
+          <Button
+            enableShadow
+            style={{ width: "90%", height: 80 }}
+            borderRadius={5}
+            label={"Submit"}
+            disabled={!amount || !picture}
+            onPress={() => onSubmit()}
+          />
         </View>
       </TouchableWithoutFeedback>
-    );
-  }
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 250,
-    padding: 10,
     alignItems: "center",
-    justifyContent: "space-evenly",
+    justifyContent: "space-around",
     flexDirection: "column"
   },
   textField: {
@@ -131,7 +161,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10
   },
   camera: {
-    flex: 0.5,
+    height: "60%",
     width: "100%"
   },
   buttonContainer: {
@@ -148,7 +178,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: "bold",
-    color: "white"
+    textAlign: "center"
   },
   inputs: {
     display: "flex",
---
+++ components/ImageViewer.tsx
@@ -0,0 +1,37 @@
+import { Image, View } from "react-native";
+import React from "react";
+import { Button } from "react-native-ui-lib";
+import { AntDesign } from "@expo/vector-icons";
+
+type Props = {
+  path: string;
+  onRemove: () => void;
+};
+
+const ImageViewer = ({ path, onRemove }: Props) => {
+  return (
+    <View style={{ position: "relative", width: "90%", height: "33%" }}>
+      {path && (
+        <>
+          <Button
+            round={true}
+            iconSource={() => <AntDesign onPress={onRemove} name="close" size={24} color="white" />}
+            size={"small"}
+            style={{
+              backgroundColor: "#ff2442",
+              position: "absolute",
+              width: 34,
+              height: 34,
+              top: -10,
+              right: -10,
+              zIndex: 1
+            }}
+          />
+          <Image style={{ width: "100%", height: "100%" }} source={{ uri: path }} />
+        </>
+      )}
+    </View>
+  );
+};
+
+export default ImageViewer;
