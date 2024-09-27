bd36c780aa60125ab4e20c804f6a27c3ca81946f
Sergio Camiño <scaminoregueiro@gmail.com>
2024-09-27 09:02:10 +0200

    removed unused stuff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,4 @@
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 import "react-native-reanimated";
-import { FontAwesome } from "@expo/vector-icons";
-import { useFonts } from "expo-font";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/lib/queryClient";
@@ -10,30 +6,7 @@ import AuthProvider from "@/components/AuthContext";
 import Toast from "react-native-toast-message";
 import { Slot } from "expo-router";
 
-export { ErrorBoundary } from "expo-router";
-
-SplashScreen.preventAutoHideAsync();
-
 const RootLayout = () => {
-  const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome.font
-  });
-
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <GestureHandlerRootView>
       <QueryClientProvider client={queryClient}>
